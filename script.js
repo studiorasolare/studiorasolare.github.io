b9b8474c07fc942aa9c85cf637a2c041caf3d17c
Anna Maria <123685928+annalewke@users.noreply.github.com>
2024-04-18 15:25:21 +0200

    Create cloud text and toggle function

---
+++ script.js
@@ -0,0 +1,24 @@
+
+// Widgets
+
+// Cloud Toggle
+
+let isCloudy = false;
+let cloudDiv = document.querySelector(".cloud-toggle");
+
+do {
+    cloudDiv.textContent = "Clouds";
+} while (isCloudy = false);
+
+// function cloudToggle() {
+// if (isCloudy = false){
+//     isCloudy = true;
+//     cloudDiv.textContent = "Clear Sky";
+// } else {
+//     isCloudy = false;
+// }
+// }
+
+// Location
+
+let locationDiv = document.querySelector(".location")
