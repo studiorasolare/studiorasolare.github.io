e9f75de7519a8199cc7f5e5a61ce74b6f8f61663
Anna Maria <123685928+annalewke@users.noreply.github.com>
2024-04-19 15:00:05 +0200

    Add Time update for current location

--- script.js
+++ script.js
@@ -3,18 +3,40 @@
 
 // Time
 
-const timeDiv = document.querySelector("#time");
-const dateDiv = document.querySelector(".time");
-const locationDiv = document.querySelector(".location");
-const defaultPlace = "milan";
-let currentPlace = defaultPlace;
+const timeDiv = document.getElementById("time");
+const dateDiv = document.getElementById("date");
+const locationDiv = document.getElementById("location");
 
+const people = document.querySelectorAll(".person");
 
-// while (currentPlace = defaultPlace) {
-    
-// };
+const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 
+const updateTime = function () {
+    let currentTime = luxon.DateTime.now().setZone("Europe/Rome").toFormat("HH:mm:ss");
+    timeDiv.innerHTML = currentTime;
+    let currLocation = "Milan, Italy";
+    locationDiv.innerHTML = currLocation;
 
+    people.forEach(person => {
+    let timezone = person.getAttribute("data-timezone");
+    let location = person.getAttribute("data-location");
+    person.addEventListener("click", ()=>{
+        currentTime = luxon.DateTime.now().setZone(timezone).toFormat("HH:mm:ss");
+        currLocation = location;
+        console.log("clicked" + location + "" + currentTime);
+    })
+    });
+}
+
+updateTime();
+
+setInterval(function () {
+    updateTime()
+}, 1000);
+
+// person.addEventListener("click", ()=> {     
+// })
 
 // Cloud Toggle
 
